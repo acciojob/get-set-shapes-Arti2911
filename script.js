e6423ed9a4a08041f91da451c56d28a8b5fced8d
Arti2911 <139011882+Arti2911@users.noreply.github.com>
2025-01-30 17:13:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width,height){
+		this._width=width;
+		this.height=height;
+	}
+	get getArea(){
+		return this._width * this._height;
+	}
+	
+}
 
-class Square extends Animal {}
+class Square extends Rectangle {
+	constructor(side){
+		this._side=side;
+	}
+	get getPerimeter(){
+		return 4 * this._side;
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
